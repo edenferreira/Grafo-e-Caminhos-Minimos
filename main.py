474c43a9610b07618d3b39c832e3714e697047f8
edenferreria <thiago_eden@hotmail.com>
2014-05-11 16:37:49 -0300

    mudando de uniform para randint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__author__ = 'Ã‰den Thiago Ferreira'
+__author__ = 'Éden Thiago Ferreira'
 from coletar_dados import *
-from random import uniform
+from random import uniform, randint
 
 for i in range(10):
-    executar_coleta(uniform(800,1500),uniform(800,1500),uniform(800,1200),uniform(800,1200),uniform(0.04,0.1))
+    executar_coleta(randint(800,1500),randint(800,1500),uniform(800,1200),uniform(800,1200),uniform(0.04,0.1))
